2674dd2fa4bd66c610a096323e8e1361b6b75641
Ivan Nečas <inecas@redhat.com>
2019-03-05 17:03:37 +0100

    Refs #26110 - authorize the session endpoint
    Since the `/pubkey` endpoint is not authorized, we need to add an ability
    to smart proxy https://github.com/theforeman/smart-proxy/pull/639 and
    then we can use `do_authorize` method at the beginning of our protected
    method.

--- lib/smart_proxy_remote_execution_ssh/api.rb
+++ lib/smart_proxy_remote_execution_ssh/api.rb
@@ -3,10 +3,14 @@ require 'net/ssh'
 module Proxy::RemoteExecution
   module Ssh
     class Api < ::Sinatra::Base
+      include Sinatra::Authorization::Helpers
+
       get "/pubkey" do
         File.read(Ssh.public_key_file)
       end
+
       post "/session" do
+        do_authorize
         if env["HTTP_CONNECTION"] != "upgrade" or env["HTTP_UPGRADE"] != "raw"
           return [ 400, "Invalid request: /ssh/session requires connection upgrade to 'raw'" ]
         end
